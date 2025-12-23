fd83a6f09da62ef1f1c814e7a8f11cf46f124738
Adam Byrne <adamrbyrne@gmail.com>
2025-12-23 15:32:02 +0000

    feat(bin, lib): add ray generation and vw mgmt

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,43 @@
-open Lumen 
+open Lumen
+open View
 
- let () =  
-  let img = Ppm.create 100 100 in 
-  (* Quadrants 1=green, 2=red, 3=blue, 4=white *)
-  for y = 0 to 99 do 
-    for x = 0 to 99 do 
-      let color = 
-        if x < 50 && y < 50 then Vec.create 0.0 1.0 0.0
-        else if x >= 50 && y < 50 then Vec.create 1.0 0.0 0.0
-        else if x < 50 && y >= 50 then Vec.create 0.0 0.0 1.0
-        else Vec.create 1.0 1.0 1.0
-      in
-      Ppm.edit_px img x y color
+let ray_for_pixel viewport camera_center i j =
+  let pixel_center =
+    viewport.upper_left
+    |> Vec.add (Vec.scale viewport.du (float_of_int i +. 0.5))
+    |> Vec.add (Vec.scale viewport.dv (float_of_int j +. 0.5))
+  in
+
+  pixel_center
+  |> Vec.sub camera_center
+  |> Vec.normalize
+
+let () =
+  let image =
+    let aspect_ratio = 16.0 /. 9.0 in
+    let width = 400 in
+    let height = int_of_float (float_of_int width /. aspect_ratio) in
+    { width; height }
+  in
+
+  let camera_center = Vec.create 0.0 0.0 0.0 in
+  let viewport = make_viewport ~image ~focal_length:1.0 in
+
+  let img = Ppm.create image.width image.height in
+
+  for j = 0 to image.height - 1 do
+    for i = 0 to image.width - 1 do
+      let dir = ray_for_pixel viewport camera_center i j in
+      let _ray = Ray.create camera_center dir in
+
+      (* simple sky gradient *)
+      let t = 0.5 *. (dir.y +. 1.0) in
+      let c1 = Vec.scale (Vec.create 1.0 1.0 1.0) (1.0 -. t) in
+      let c2 = Vec.scale (Vec.create 0.5 0.7 1.0) t in
+
+      Ppm.edit_px img i j (Vec.add c1 c2)
     done
   done;
+
   Ppm.save "output.ppm" img
 
---
+++ lib/view.ml
@@ -0,0 +1,53 @@
+open Vec 
+
+type viewport = {
+  width : float;
+  height : float;
+  focal_length : float;
+  upper_left : vec;
+  u : vec;          (* horizontal axis *)
+  v : vec;          (* vertical axis *)
+  du : vec;         (* pixel delta in u *)
+  dv : vec;         (* pixel delta in v *)
+}
+
+type image = {
+  width : int;
+  height : int;
+}
+
+let make_viewport ~image ~focal_length =
+  let aspect_ratio =
+    float_of_int image.width /. float_of_int image.height
+  in
+
+  let viewport_height = 2.0 in
+  let viewport_width = aspect_ratio *. viewport_height in
+
+  let camera_center = create 0.0 0.0 0.0 in
+
+  let u = create viewport_width 0.0 0.0 in
+  let v = create 0.0 viewport_height 0.0 in
+
+  let upper_left =
+    camera_center
+    |> sub (Vec.scale u 0.5)
+    |> sub (Vec.scale v 0.5)
+    |> sub (Vec.create 0.0 0.0 focal_length)
+  in
+
+  let du = scale u (1.0 /. float_of_int image.width) in
+  let dv = scale v (1.0 /. float_of_int image.height) in
+
+  {
+    width = viewport_width;
+    height = viewport_height;
+    focal_length;
+    upper_left;
+    u;
+    v;
+    du;
+    dv;
+  }
+
+
