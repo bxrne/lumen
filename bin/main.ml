71f670e74ac109e247629851d0a465393d9819e0
Adam Byrne <adamrbyrne@gmail.com>
2025-12-23 01:40:25 +0000

    feat(*): Create PPM images

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+open Lumen 
+
+let gradient x y =
+  let r = (x * 255) / 99 in
+  let g = (y * 255) / 99 in
+  let b = 128 in
+  { Ppm.r = r; g = g; b = b }
+
+let () = 
+  let img = Ppm.create_image 100 100 in 
+  for y = 0 to img.height - 1 do 
+    for x = 0 to img.width - 1 do 
+      let color = gradient x y in
+      Ppm.set_pixel img x y color
+    done
+  done;
+  Ppm.write_ppm "output.ppm" img
+
---
+++ lib/ppm.ml
@@ -0,0 +1,27 @@
+(* Represent image via the PPM format *)
+
+type color = { r: int; g: int; b: int }
+type image = { width: int; height: int; pixels: color array array }
+
+let create_image width height =
+  let pixels = Array.init height (fun _ -> Array.init width (fun _ -> { r = 0; g = 0; b = 0 })) in
+  { width; height; pixels }
+
+let set_pixel img x y color =
+  if x >= 0 && x < img.width && y >= 0 && y < img.height then
+    img.pixels.(y).(x) <- color 
+  else
+    failwith "Pixel coordinates out of bounds"
+
+let write_ppm filename img =
+  let oc = open_out filename in 
+  Printf.fprintf oc "P3\n%d %d\n255\n" img.width img.height;
+  for y = 0 to img.height - 1 do 
+    for x = 0 to img.width - 1 do 
+      let c = img.pixels.(y).(x) in
+      Printf.fprintf oc "%d %d %d " c.r c.g c.b
+    done;
+    Printf.fprintf oc "\n"
+  done;
+  close_out oc 
+
