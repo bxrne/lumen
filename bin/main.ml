fc23da2e9c10852ae4ffc6f1972715058aec380a
Adam Byrne <adamrbyrne@gmail.com>
2025-12-23 20:21:43 +0000

    fix(*): coordinates were getting messed up

--- bin/main.ml
+++ bin/main.ml
@@ -8,10 +8,7 @@ let ray_for_pixel viewport camera_center i j =
     |> Vec.add (Vec.scale viewport.du (float_of_int i +. 0.5))
     |> Vec.add (Vec.scale viewport.dv (float_of_int j +. 0.5))
   in
-
-  pixel_center
-  |> Vec.sub camera_center
-  |> Vec.normalize
+  Vec.normalize (Vec.sub pixel_center camera_center)
 
 let () =
   let image =
@@ -24,35 +21,23 @@ let () =
   let camera_center = Vec.create 0.0 0.0 0.0 in
   let viewport = make_viewport ~image ~focal_length:1.0 in
   
-  (* Debug viewport *)
-  Printf.printf "Viewport upper_left: (%.3f, %.3f, %.3f)\n" 
-    viewport.upper_left.x viewport.upper_left.y viewport.upper_left.z;
-
   (* Create a sphere at origin (0,0,-2) with radius 0.5 *)
   let sphere = Shapes.create_sphere ~center:(Vec.create 0.0 0.0 (-2.0)) ~radius:0.5 in
-  
-  Printf.printf "Sphere center: (%.3f, %.3f, %.3f), radius: %.3f\n" 
-    sphere.center.x sphere.center.y sphere.center.z sphere.radius;
 
   let img = Ppm.create image.width image.height in
   let hit_count = ref 0 in
 
-  (* Test center ray specifically *)
-  let center_i = image.width / 2 in
-  let center_j = image.height / 2 in
-  let center_dir = ray_for_pixel viewport camera_center center_i center_j in
-  let center_ray = Ray.create camera_center center_dir in
-  let center_hit = hit_sphere sphere center_ray in
-  
-  Printf.printf "Center pixel (%d, %d): ray=(%.3f,%.3f,%.3f) hit=%b\n" 
-    center_i center_j center_dir.x center_dir.y center_dir.z center_hit;
+
 
   for j = 0 to image.height - 1 do
     for i = 0 to image.width - 1 do
       let dir = ray_for_pixel viewport camera_center i j in
       let ray = Ray.create camera_center dir in
 
-      let hit = hit_sphere sphere ray in
+      let hit_dist = hit_sphere sphere ray in
+      let hit = hit_dist >= 0.0 in
+      
+
       
       if hit then
         hit_count := !hit_count + 1;
--- lib/shapes.ml
+++ lib/shapes.ml
@@ -13,14 +13,15 @@ let create_sphere ~center ~radius = {
 }
 
 (* If roots exist, the ray hits the sphere *)
-let hit_sphere (s: sphere) (r: ray) : bool =
+let hit_sphere (s: sphere) (r: ray) : float =
   let oc = sub r.origin s.center in
   let a = dot r.direction r.direction in
   let b = 2.0 *. dot oc r.direction in
   let c = dot oc oc -. s.radius *. s.radius in
   let discriminant = b *. b -. 4.0 *. a *. c in
-  discriminant > 0.0
+  if discriminant < 0.0 then -1.0 else (-.b -. sqrt discriminant) /. (2.0 *. a)
 
 (* Compute normal at point p on the sphere's surface *)
 let sphere_normal (s: sphere) (p: vec) : vec =
   normalize (Vec.sub p s.center)
+
